9586d0907b0f38e7f9b0ea98abc55a5562946b52
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-20 20:03:35 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     
-    int findAns( int currStairs, int n, vector<int>&dp )
-    {
-        if ( currStairs == n )  return 1 ;
-        if ( currStairs > n )   return 0 ;
+//     int findAns( int currStairs, int n, vector<int>&dp )
+//     {
+//         if ( currStairs == n )  return 1 ;
+//         if ( currStairs > n )   return 0 ;
         
-        if ( dp[currStairs] != -1 )  return dp[currStairs] ;
+//         if ( dp[currStairs] != -1 )  return dp[currStairs] ;
         
-        int oneStep = findAns( currStairs + 1, n, dp ) ;
-        int twoStep = findAns( currStairs + 2, n, dp ) ;
+//         int oneStep = findAns( currStairs + 1, n, dp ) ;
+//         int twoStep = findAns( currStairs + 2, n, dp ) ;
         
-        dp[currStairs] = oneStep + twoStep ;
-        return oneStep + twoStep ;
+//         dp[currStairs] = oneStep + twoStep ;
+//         return oneStep + twoStep ;
         
-    }
+//     }
     
     int climbStairs(int n) 
     {
         vector<int>dp(n+1,-1) ;
-        return findAns(0, n, dp ) ;
+        dp[1] = 1 ;
+        dp[0] = 1 ;
+        
+        
+        //return findAns(0, n, dp ) ;
+        
+        
+        for ( int i = 2 ; i <= n ; i++ )
+        {
+            dp[i] = dp[i-1] + dp[i-2] ;
+        }
+        return dp[n] ;
     }
 };
