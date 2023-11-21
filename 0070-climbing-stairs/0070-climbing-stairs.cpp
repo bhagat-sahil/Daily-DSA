73dbf072e900cbce9c12bb37fd3fda0272956c02
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-21 10:28:07 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (55.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    
-//     int findAns( int currStairs, int n, vector<int>&dp )
-//     {
-//         if ( currStairs == n )  return 1 ;
-//         if ( currStairs > n )   return 0 ;
-        
-//         if ( dp[currStairs] != -1 )  return dp[currStairs] ;
-        
-//         int oneStep = findAns( currStairs + 1, n, dp ) ;
-//         int twoStep = findAns( currStairs + 2, n, dp ) ;
-        
-//         dp[currStairs] = oneStep + twoStep ;
-//         return oneStep + twoStep ;
-        
-//     }
-    
+
+    //  SPACE OPTIMISATION
+    //
     int climbStairs(int n) 
     {
-        vector<int>dp(n+1,-1) ;
-        dp[1] = 1 ;
-        dp[0] = 1 ;
-        
-        
-        //return findAns(0, n, dp ) ;
-        
+        int prev = 1 ;
+        int prev2 = 1 ;
+        int curr ;
         
         for ( int i = 2 ; i <= n ; i++ )
         {
-            dp[i] = dp[i-1] + dp[i-2] ;
+            curr = prev + prev2 ;
+            prev2 = prev ;
+            prev = curr ;
         }
-        return dp[n] ;
+     
+        return prev ;
     }
 };
