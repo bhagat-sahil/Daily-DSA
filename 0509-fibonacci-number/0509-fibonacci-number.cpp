10bbb373efdfd3aea95af248975c09e2114ec04e
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-23 11:40:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (10.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    
-    int findfibo( int n, vector<int>&dp )
-    {
-        if ( n <= 1 )   return n ;
-        
-        if( dp[n] != -1 )   return dp[n] ;
-        
-        return dp[n] = findfibo(n-1, dp ) + findfibo(n-2, dp) ;
-    }
-    
+
     int fib(int n) 
     {
+        if( n == 0 || n == 1 )  return n ;
         vector<int>dp(n+1,-1) ;
-        return findfibo(n, dp) ;
+        dp[0] = 0 ;
+        dp[1] = 1 ;
+        for ( int i = 2 ; i <= n ; i++ )
+        {
+            dp[i] = dp[i-1] + dp[i-2] ;
+        }
+        return dp[n] ;
     }
 };
