27223791ac9c4cf9c13561d57c1470b496eec4f4
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-23 15:02:04 +0530

    Time: 3 ms (35.91%), Space: 8 MB (84.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,14 +4,18 @@ public:
     {
         int n = nums.size() ;
         if( n == 1 ) return nums[0] ;
-        vector<int>dp(n+1,-1) ;
-        dp[0] = nums[0] ;
-        dp[1] = max( nums[0], nums[1] ) ;
+        //vector<int>dp(n+1,-1) ;
+        
+        int curr ;
+        int prev2 = nums[0] ;
+        int prev = max( nums[0], nums[1] ) ;
         
         for ( int i = 2 ; i < n ; i++ )
         {
-            dp[i] = max( dp[i-1], nums[i] + dp[i-2] ) ;
+            curr = max( prev, nums[i] + prev2 ) ;
+            prev2 = prev ;
+            prev = curr ;
         }
-        return max(dp[n-1],dp[n-2]) ;
+        return max(curr,prev) ;
     }
 };
