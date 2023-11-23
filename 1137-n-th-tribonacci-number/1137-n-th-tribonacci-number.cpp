92af91112eb4649eb6c3863dee542342c1fab3b9
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-23 12:21:39 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (43.13%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     
-    int findTribo( int n, vector<int>&dp )
-    {
-        if ( n == 0 ) return 0 ;
-        if ( n <= 2 )   return 1 ;
-        
-        
-        if ( dp[n] != -1 )  return dp[n] ;
-        
-        return dp[n] = findTribo( n-1, dp ) + findTribo( n-2, dp ) + findTribo( n-3, dp ) ;
-        
-    }
-    
     
     
     int tribonacci(int n) 
     {
+        if ( n == 0 ) return 0 ;
+        if ( n <= 2 )   return 1 ;
         vector<int>dp( n + 1, -1 ) ;
-        return findTribo( n, dp ) ;
+        dp[0] = 0 ;
+        dp[1] = 1 ;
+        dp[2] = 1 ;
+        
+        for ( int i = 3 ; i <= n ; i++ )
+        {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3] ;
+        }
+        
+        return dp[n] ;
         
     }
 };
