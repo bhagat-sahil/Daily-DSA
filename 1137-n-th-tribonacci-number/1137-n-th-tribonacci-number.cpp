a6ee8a004445f4da1e8146ab6b8a91d11e682b5c
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-23 12:25:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (64.48%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -7,17 +7,22 @@ public:
     {
         if ( n == 0 ) return 0 ;
         if ( n <= 2 )   return 1 ;
-        vector<int>dp( n + 1, -1 ) ;
-        dp[0] = 0 ;
-        dp[1] = 1 ;
-        dp[2] = 1 ;
+        //vector<int>dp( n + 1, -1 ) ;
+        
+        int prev = 1 ;
+        int prev2 = 1 ;
+        int prev3 = 0 ;
+        int curr ;
         
         for ( int i = 3 ; i <= n ; i++ )
         {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3] ;
+            curr = prev + prev2 + prev3 ;
+            prev3 = prev2 ;
+            prev2 = prev ;
+            prev = curr ;
         }
         
-        return dp[n] ;
+        return curr ;
         
     }
 };
