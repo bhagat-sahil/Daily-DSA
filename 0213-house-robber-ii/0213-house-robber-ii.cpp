9031251097a8595c73bde556717705c5ca7f6b9d
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2023-11-25 23:58:26 +0530

    Time: 3 ms (37.67%), Space: 8.1 MB (70.76%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    
-    int findMaxLoot( vector<int>&nums, int s, int end )
-    {
-        int n = nums.size() ;
-        vector<int>dp(n+1, -1 ) ;
-        dp[0] = nums[s] ;
-        dp[1] = max( nums[s], nums[s+1] ) ;
-        int i = s ;
-        s = 2 ;
-        while ( s < end - i + 1 )
-        {
-            if ( i == 1 )
-            {
-                dp[s] = max(dp[s-1], dp[s-2] + nums[s+1]) ;
-            }
-            else dp[s] = max(dp[s-1], dp[s-2] + nums[s]) ;
-            s++ ;
+    int findMaxLoot(vector<int>& nums, int start, int end) {
+        int prevMax = nums[start];
+        int currMax = max(nums[start], nums[start + 1]);
+
+        for (int i = start + 2; i <= end; ++i) {
+            int temp = currMax;
+            currMax = max(currMax, prevMax + nums[i]);
+            prevMax = temp;
         }
-        return max(dp[n-1],dp[n-2]) ;
-    
+
+        return currMax;
     }
-    
-    int rob(vector<int>& nums) 
-    {
-        if ( nums.size() == 1 ) return nums[0] ;
-        if ( nums.size() == 2 ) return max(nums[0],nums[1]) ;
-        return max(findMaxLoot( nums, 0, nums.size()-2 ), findMaxLoot( nums, 1, nums.size()-1 )) ;
-        //return findMaxLoot( nums, 0, nums.size()-2 ) ;
-        //return findMaxLoot( nums, 1, nums.size()-1 ) ;
+
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+
+        int max1 = findMaxLoot(nums, 0, nums.size() - 2);
+        int max2 = findMaxLoot(nums, 1, nums.size() - 1);
+
+        return max(max1, max2);
     }
 };
