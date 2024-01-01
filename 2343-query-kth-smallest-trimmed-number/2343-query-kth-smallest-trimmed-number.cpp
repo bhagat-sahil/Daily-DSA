77abcb822a371cf60aab0888802e816ee2f13116
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-01-01 18:15:53 +0530

    Time: 2375 ms (10.82%), Space: 373.1 MB (57.73%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        vector<int> res;
-        int n = nums.size();
-        for(auto &q:queries) {
-            vector<pair<string,int>> pq;
-            for(int i=0;i<n; i++) {
-                pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries)
+    {
+        vector<int>ans;
+        
+        
+        for(auto current : queries)
+        {
+            int n = current[0];
+            int len = current[1];
+            vector<pair<string,int>>arr; // number : index
+            for(int i = 0; i < nums.size(); i++)
+            {
+                // string number = nums[i];
+                // int numberLen = number.size();
+                // string str = number.substr(numberLen-len);
+                arr.push_back({nums[i].substr(nums[i].size()-len),i});
             }
-            sort(pq.begin(), pq.end());
-            res.push_back(pq[q[0]-1].second);
+            sort(arr.begin(),arr.end());
+            ans.push_back(arr[n-1].second);
         }
-        return res;
+        return ans;
     }
 };
+
