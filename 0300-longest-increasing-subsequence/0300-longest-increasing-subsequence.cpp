c628c08605a8e3496436e8ec7dc0534dd9580fc5
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-01-13 23:41:22 +0530

    Time: 252 ms (32.14%), Space: 11.1 MB (28.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -26,19 +26,33 @@ public:
     int lengthOfLIS(vector<int>& nums) 
     {
         int n = nums.size() ;
-        vector<int>dp(n+1,1) ;
-        int maxi = 0 ;
+        vector<int>dp(n+1,1), hash(n+1,0) ;
+        int maxi = 1 ;
+        int lastIdx = 0 ;
         for ( int i = 0 ; i < n ; i++ )
         {
             for ( int prev = 0 ; prev < i ; prev++ )
             {
-                if ( nums[i] > nums[prev])
+                if ( nums[i] > nums[prev] && dp[i] < 1+ dp[prev] )
                 {
-                    dp[i] = max( dp[i], 1+ dp[prev] ) ;
+                    dp[i] = 1+ dp[prev]  ;
+                    hash[i] = prev ;
                 }
             }
-            maxi = max( maxi, dp[i]) ;
+            if ( dp[i] > maxi )
+            {
+                maxi = dp[i] ;
+                lastIdx = i ;
+            }
         }
+        vector<int>temp ;
+        temp.push_back(nums[lastIdx]) ;
+        while ( hash[lastIdx] != lastIdx )
+        {
+            lastIdx = hash[lastIdx] ;
+            temp.push_back(nums[lastIdx]) ;
+        }
+        reverse( temp.begin(),temp.end()) ;
         return maxi ;
     }
 };
