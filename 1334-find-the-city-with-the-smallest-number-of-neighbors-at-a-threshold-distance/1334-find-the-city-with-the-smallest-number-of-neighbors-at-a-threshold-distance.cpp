d82bd27fad9a486dbbe3b321d1833c0d27ec3fa2
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-01-21 19:57:23 +0530

    Time: 27 ms (93.29%), Space: 15.8 MB (21.32%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -6,54 +6,52 @@ public:
         
         for(auto current : edges)
         {
-            int to = current[0];
-            int from = current[1];
-            int weight = current[2];
+            int to = current[0] ;
+            int from = current[1] ;
+            int weight = current[2] ;
             
-            distance[to][from] = weight;
-            distance[from][to] = weight; // since its biderectional
+            distance[to][from] = weight ;
+            distance[from][to] = weight ; 
         }
         
-        for(int intermediate = 0; intermediate < n; intermediate++)
+        for( int m = 0 ; m < n ; m++ )
         {
-            for(int source = 0; source < n; source++)
+            for(int source = 0 ; source < n ; source++ )
             {
-                for(int destination = 0; destination < n; destination++)
+                for(int destination = 0 ; destination < n ; destination++ )
                 {
-                    if(source == destination)
+                    if( source == destination )
                     {
-                        distance[source][destination] = 0;
+                        distance[source][destination] = 0 ;
                         continue;
                     }
-                    if(distance[source][intermediate] != INT_MAX && distance[intermediate][destination] != INT_MAX)
+                    if( distance[source][m] != INT_MAX && distance[m][destination] != INT_MAX )
                     {
                         distance[source][destination] = min(distance[source][destination],
-                                                         distance[source][intermediate]+distance[intermediate][destination]);
+                                                         distance[source][m]+distance[m][destination]);
                     }
                 }
             }
         }
         
         vector<int>cityCount(n,0);
-        for(int i = 0; i < n; i++)
+        for( int i = 0 ; i < n ; i++ )
         {
-            for(int j = 0; j < n; j++)
+            for( int j = 0 ; j < n ; j++ )
             {
-                // cout<<distance[i][j]<<" ";
-                if(distance[i][j] <= distanceThreshold)
-                    cityCount[i]++;
+                if( distance[i][j] <= distanceThreshold )
+                    cityCount[i]++ ;
             }
-            // cout<<endl;
         }
         
         int ans = 0;
         int minCount = INT_MAX;
-        for(int index = 0; index < n; index++)
+        for( int i = 0 ; i < n ; i++ )
         {
-            if(cityCount[index] <= minCount)
+            if( cityCount[i] <= minCount )
             {
-                minCount = cityCount[index];
-                ans = index;
+                minCount = cityCount[i] ;
+                ans = i ;
             }
         }
         return ans;
