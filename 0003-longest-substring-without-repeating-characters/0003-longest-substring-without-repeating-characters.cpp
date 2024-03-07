7c59281a07a92816085fc2cab188735216f4b265
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-03-07 11:52:42 +0530

    Time: 11 ms (71.16%), Space: 11.1 MB (64.58%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,18 +7,18 @@ public:
         unordered_map<char,int>mp ;
         while ( r < s.size() )
         {
-            if ( mp.count(s[r]) == 0 )
+            if ( mp[s[r]] == 0 )
             {
                 mp[s[r]]++ ;
                 ans = max( ans, r - l + 1 ) ;
             }
             else
             {
-                while ( mp.count(s[r]) != 0 )
+                while ( mp[s[r]] != 0 )
                 {
                     mp[s[l]]-- ;
-                    if ( mp[s[l]] == 0 )
-                        mp.erase(s[l]) ;
+                    // if ( mp[s[l]] == 0 )
+                    //     mp.erase( s[l] ) ;
                     l++ ;
                 }
                 mp[s[r]]++ ;
