da666f86260c01061b089306d497e1f81b8c403e
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-04-14 14:20:38 +0530

    Time: 4 ms (44.55%), Space: 11 MB (8.04%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -8,31 +8,24 @@ public:
         string ans = "" ;
         while ( i >= 0 && j >= 0 )
         {
-            cout<<int(a[i]-'0')<<" " ;
-            cout<<int(b[j]-'0')<<" " ;
-            cout<<c<<endl ;
             
             if ( int(a[i]-'0') + int(b[j]-'0') + c == 0 )
             {
-                 cout<<"1"<<endl ;
                 c = 0 ;
                 ans = "0" + ans ;
             }
             else if ( int(a[i]-'0') + int(b[j]-'0') + c == 1 )
             {
-                cout<<"2"<<endl ;
                 c = 0 ;
                 ans = "1" + ans ;
             }
             else if ( int(a[i]-'0') + int(b[j]-'0') + c == 2 )
             {
-                cout<<"3"<<endl ;
                 c = 1 ;
                 ans = "0" + ans ;
             }
             else if ( int(a[i]-'0') + int(b[j]-'0') + c == 3 )
             {
-                cout<<"4"<<endl ;
                 c = 1 ;
                 ans = "1" + ans ;
             }
