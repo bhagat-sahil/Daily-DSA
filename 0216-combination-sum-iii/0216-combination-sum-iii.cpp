194cb1f705bade8c59f5e043dc06066e928993d4
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-04-20 13:39:37 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (44.80%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     
-    void findCombinations( int i, int numCount, int target, vector<int>&combi, vector<vector<int>>&ans )
+    
+    void findCombo( int num, int k, int n, vector<int>&temp, vector<vector<int>>&ans )
     {
-        if ( target == 0 && numCount == 0 ){
-            ans.push_back(combi) ;
+        if ( k == 0 && n == 0 )
+        {
+            ans.push_back(temp) ;
             return ;
         }
-        if ( numCount == 0 || i > 9 )   return ;
         
-        //TAKE
-        if ( target - i >= 0 )
-        {
-            combi.push_back(i) ;
-            findCombinations( i+1, numCount-1, target-i, combi, ans ) ;
-            combi.pop_back() ;
-        }
-        //NOT TAKE
-        findCombinations( i+1, numCount, target, combi, ans ) ;
+        if ( n < 0 || k == 0 || num > n || num > 9 )
+            return ;
+        
+        temp.push_back(num) ;
+        findCombo( num+1, k-1, n-num, temp, ans ) ;
+        temp.pop_back() ;
+        
+        findCombo( num+1, k, n, temp, ans ) ;
         
     }
-
+    
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>combi ;
+        
         vector<vector<int>> ans ;
-        findCombinations( 1, k, n, combi, ans ) ;
+        vector<int> temp ;
+            
+        findCombo ( 1, k, n, temp, ans ) ;
         return ans ;
     }
 };
