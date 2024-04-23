0beb4bfa9a8672b5141e5a1fb9cf4be235437394
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-04-23 23:45:04 +0530

    Time: 103 ms (95.84%), Space: 64.3 MB (71.83%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        vector<int>t1( n+1, 0 ) ; 
+        vector<int>t2( n+1, 0 ) ;
+
+        for (int i = 0 ; i < trust.size() ; i++ )
+        {
+            t1[trust[i][0]]++ ;
+            t2[trust[i][1]]++ ;
+        }
+
+        int ans = -1 ;
+
+        for ( int i = 1 ; i <= n ; i++ )
+        {
+            if( t1[i] == 0 && t2[i] == n-1 )
+                ans = i ;
+        }
+
+        return ans ;
+
+    }
+};
