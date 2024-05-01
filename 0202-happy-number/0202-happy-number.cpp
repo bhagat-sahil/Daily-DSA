02cca6e8f60f64081e1267e7d1d1246ac1f36687
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-05-01 08:44:46 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (56.87%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool isHappy(int n) {
         
-        if ( n == 1 || n == 7 ) 
-            return true ;
+//         if ( n == 1 || n == 7 ) 
+//             return true ;
         
-        if ( n < 10 )
-            return false ;
+//         if ( n < 10 )
+//             return false ;
         
         while ( n > 10 )
         {
