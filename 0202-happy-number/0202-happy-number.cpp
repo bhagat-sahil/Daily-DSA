31754be1b3310e7b165607271d9773760ec2f1c7
Sahil Bhagat <114816471+bhagat-sahil@users.noreply.github.com>
2024-05-01 08:41:35 +0530

    Time: 3 ms (40.52%), Space: 7.4 MB (55.53%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        if ( n == 1 || n == 7 ) 
+            return true ;
+        
+        if ( n < 10 )
+            return false ;
+        
+        while ( n > 10 )
+        {
+            int temp = 0 ;
+            string s = to_string ( n ) ;
+            for ( auto it : s )
+            {
+                temp = temp + pow((it-'0'),2) ;
+            }
+            n = temp ;
+        }
+        return n == 1 || n == 7 || n == 10 ;
+    }
+};
